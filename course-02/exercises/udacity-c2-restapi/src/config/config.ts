49f1499cb5f0231c37ab1c47215cc345f7ae2a88
Mike Jackson <jackson-mike@sbcglobal.net>
2020-01-30 15:59:32 -0500

    Before todos to work

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "jacksonudagram",
+    "password": "jacksonudagram",
+    "database": "jacksonudagram",
+    "host": "jacksonudagram.cm4xlcca34ef.us-east-2.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "jacksonudagram"
   },
   "prod": {
     "username": "",
