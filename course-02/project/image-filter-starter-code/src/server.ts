b84ed8886ee2d7f26ff6ad4805ccdfb57407df06
Mike Jackson <jackson-mike@sbcglobal.net>
2020-02-15 21:13:05 -0500

    First change

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -28,7 +28,8 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-
+  //  This is a change
+  
   //! END @TODO1
   
   // Root Endpoint
