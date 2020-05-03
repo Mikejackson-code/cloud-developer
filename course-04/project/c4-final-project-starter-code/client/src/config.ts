101f50f755bfeba451b4da0acbb4e23120f31b91
Mike Jackson <jackson-mike@sbcglobal.net>
2020-05-03 11:18:53 -0400

    new attempt to connec to todo-serverless

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'bxoy31h27j'
+const apiId = '4ud1dyvl3g'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'dev-0hz4le1t.auth0.com',            // Auth0 domain
-  clientId: 'dST1oBrZT93fev8LH4wXMG0N0Gm8mGsv',          // Auth0 client id
+  clientId: 'ea4vzNzWlzCmgqsGX3KEsOFaWxjx6S3a',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
